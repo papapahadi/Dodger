25d86a864e24d938cac0173db79520f81e811c6b
Jatin Singh <singhjatin1812@gmail.com>
2026-02-15 14:16:11 +0530

    Initial Commit

---
+++ Dodger/DodgerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DodgerApp.swift
+//  Dodger
+//
+//  Created by Jatin Singh on 15/02/26.
+//
+
+import SwiftUI
+
+@main
+struct DodgerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DodgerTests/DodgerTests.swift
@@ -0,0 +1,17 @@
+//
+//  DodgerTests.swift
+//  DodgerTests
+//
+//  Created by Jatin Singh on 15/02/26.
+//
+
+import Testing
+@testable import Dodger
+
+struct DodgerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
